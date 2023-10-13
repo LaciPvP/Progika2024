d9425531b41eb603204e61a88df4062ae8190afd
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 08:44:07 +0200

    szokereso10.13

---
+++ Asztali alkalmazások/Tombgyak10.13/Tombgyak10.13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Kérek egy számot: ");
+int beker = Convert.ToInt32(Console.ReadLine());
+
+
+string[] szavak = new string[beker];
+for (int i = 0; i < beker; i++) 
+{
+    Console.Write("Kérem a szavakat: ");
+    szavak[i] = Console.ReadLine();
+
+    
+
+
+}
+
+
+
