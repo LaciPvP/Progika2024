c9206d603425d4e115fcc954668a52f0edd1508e
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-10-13 11:29:24 +0200

    Gyumolcsok

--- Asztali alkalmazások/Tombgyak10.13/Tombgyak10.13/Program.cs
+++ Asztali alkalmazások/Tombgyak10.13/Tombgyak10.13/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Kérek egy számot: ");
+﻿/*
+Console.Write("Kérek egy számot: ");
 int beker = Convert.ToInt32(Console.ReadLine());
 
 
@@ -12,6 +13,100 @@ for (int i = 0; i < beker; i++)
 
 
 }
+string max = "";
+for (int i = 0; i < szavak.Length; i++) 
+{
+    if (szavak[i].Length > max.Length) 
+    
+    {
+
+        max = szavak[i];
+    
+    }
+
+}
+Console.WriteLine("A leghosszabb szó: "+max);
+
+
+
+//index keresés
+int maxIndex = 0;
+for (int i = 0; i < szavak.Length; i++) 
+{
+    if (szavak[i] == max) 
+    {
+        maxIndex = i;
+    
+    }
+
+
+}
+
+
+
+
+
+//Maximum érték keresése másként
+max = "";
+maxIndex = 0;
+for (int i = 0; i < szavak.Length; i++) 
+{
+    if (szavak[i].Length > max.Length) 
+    {
+        max = szavak[i];
+        maxIndex = i; 
+    
+    }
+
+
+}
+
+
+
+
+
+//Maximum érték keresése másként(2)
+
+maxIndex = 0;
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length > szavak[maxIndex].Length)
+    { 
+        maxIndex = i;
+    }
+}
+*/
+
+
+
+int rIndex = 0;
+int hIndex = 0;
+
+string[] szavak = new string[] {"alma","körte","narancs","mandula","ananász","banán"};
+
+string[] rovid = new string[szavak.Length]; //5 karakter vagy annál rövidebb
+string[] hosszu = new string[szavak.Length]; //% karakternél több
+
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length <= 5)
+    {
+        rovid[rIndex] = szavak[i];
+        rIndex++;
+
+
+    }
+
+    else 
+    {
+        hosszu[hIndex++] = szavak[i];
+        hIndex++;
+    }
+
+
+}
+Console.WriteLine();
+Console.WriteLine();
 
 
 
