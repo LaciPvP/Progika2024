d6f8dea7f95ead6f2ac2d393780bc84766b0cb85
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-10-17 11:22:41 +0200

    Tömbgyakorlás 10.17 kész

--- Asztali alkalmazások/Tombgyak1017/Tombgyak1017/Program.cs
+++ Asztali alkalmazások/Tombgyak1017/Tombgyak1017/Program.cs
@@ -1,4 +1,7 @@
-﻿int[] tombkeszit(int darab,int legkisebb, int legnagyobb)
+﻿
+
+
+int[] tombkeszit(int darab,int legkisebb, int legnagyobb)
 {
     int[] tomb = new int[darab];
     Random rand = new Random();
@@ -37,17 +40,16 @@ Console.WriteLine("A számok között {0} páros, és {1} páratlan volt.", paro
 
 
 //következő szám vizsgálása
+
 bool vanEgyenloSzomszedos = false;
-for (int i = 0; i < szamok.Length-2; i++) 
+for (int i = 0; i < szamok.Length-1; i++) 
 {
-    if (szamok[i] == szamok[i + 2]) 
+    if (szamok[i] == szamok[i + 1]) 
     {
         vanEgyenloSzomszedos = true;
         break;
     
     }
-
-
 }
 if (vanEgyenloSzomszedos)
 {
@@ -58,3 +60,72 @@ else
 {
     Console.WriteLine("Nincs szomszédos elem!");
 }
+
+
+
+//előző elem vizsgálata
+bool vanEgyenloSzomszedos1 = false;
+for (int i = 1; i < szamok.Length - 1; i++)
+{
+    if (szamok[i] == szamok[i + -1])
+    {
+        vanEgyenloSzomszedos1 = true;
+        Console.WriteLine("Egyenlő(k): {0}, és {1}, és {2} = {3}", i - 1, i, szamok[i - 1], szamok[i]);
+
+    }
+}
+if (vanEgyenloSzomszedos1)
+{
+    Console.WriteLine("Van egyenlő szomszédos elem!");
+
+}
+else
+{
+    Console.WriteLine("Nincs szomszédos elem!");
+}
+
+
+
+void szakasz2(int[] szamTomb) 
+{
+    int kezd = 0;
+    int veg = 0;
+    int Maxkezd = 0;
+    int Maxveg = 0;
+
+    for (int i = 1; i < szamTomb.Length; i++) 
+    {
+        if (szamTomb[i - 1] < szamTomb[i]) 
+        {
+        
+        
+        
+        }
+
+        else
+        {
+            veg = i - 1;
+            if (veg -kezd+1>Maxveg-Maxkezd+1) 
+            {
+                Maxveg = veg;
+                Maxkezd = kezd;
+            
+            
+            }
+            kezd = i;
+
+        }
+
+
+    }
+    for (int i = Maxkezd; i < Maxveg + 1; i++) 
+    {
+        Console.WriteLine("a legnagyobb növekvő számsorozat {0} elemű: ", Maxveg-Maxkezd);
+
+
+    }
+
+    
+}
+szakasz2(szamok);
+
