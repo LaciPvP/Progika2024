74993a49886b48ac104fda296bc873c309792bd9
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-11-07 10:06:07 +0100

    HaziMegoldas (kesz)

--- Asztali alkalmazások/HaziMegooldas/HaziMegooldas/Program.cs
+++ Asztali alkalmazások/HaziMegooldas/HaziMegooldas/Program.cs
@@ -23,7 +23,8 @@ for (int i = 0; i < sokszo.Length; i++)
     sokszo[i] = szo(rand.Next(4,7));
 
 }
-
+//sokszo[sokszo.Length - 1] = "aaaa";
+//sokszo[sokszo.Length - 1] = "baaab";
 File.WriteAllLines("szavak.txt", sokszo);
 
 /* //Debugolás
@@ -49,20 +50,56 @@ for (int i = 0; i < szavak.Length; i++)
         }
     
     }
-    Console.WriteLine(tobbszoros);
-    //Console.WriteLine("Többször előforduló szavak:" +tobbszoros.Substring(1));
+    
 }
+Console.WriteLine("Többször előforduló szavak:" +tobbszoros.Substring(1));
 
 //5.feladat
 for (int i = 0; i < szavak.Length; i++) 
 {
     if (szavak[i].Length == 4) 
     {
-        szavak[i] = szavak[i].Substring(0,1).ToUpper();
+        szavak[i] = szavak[i].Substring(0,1).ToUpper()+szavak[i].Substring(1);
         Console.WriteLine(szavak[i]);
 
 
     }
 
+
+}
+
+//6. Feladat
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length == 4)
+    {
+        for (int k = 0; k < szavak.Length; k++) 
+        {
+            if (szavak[k].Length == 6) 
+            {
+                if (szavak[k].IndexOf(szavak[i].ToLower()) > -1)
+                {
+                    Console.WriteLine("{0} benne van ebben {1}" + szavak[i], szavak[k]); 
+                }
+            
+            }
+        
+        }
+    
+    }
+
+
+}
+
+//7. Feladat
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length == 5) 
+    {
+        szavak[i] = szavak[i].Replace("a", "@").Replace("e", "3").Replace("l", "1").Replace("o", "0");
+                                        
+       
+
+    }
 
 }
