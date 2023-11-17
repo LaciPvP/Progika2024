2111c41440285c514857f6e4b887a5d7aae13308
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-11-17 08:40:45 +0100

    Lista gyakorlás 11.17

---
+++ Adatbázis Kezelés/ListaGyak1117/ListaGyak1117/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> szamok = new List<int>();
+Random rand = new Random();
+
+/*int darab = rand.Next(10000,100000);*/
+
+for (int i = 0; i < rand.Next(10000,100000); i++) 
+{
+    szamok.Add(rand.Next(10,100));
+
+
+}
+Console.WriteLine("{0} darab szám van.", szamok.Count);
