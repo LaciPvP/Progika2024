b65a5813852b529205a4f2cb246e0f6d39cdb51e
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-12-12 13:39:25 +0100

    Objektum Orientált Programozás

--- Asztali alkalmazások/fejvagyiras/fejvagyiras/Program.cs
+++ Asztali alkalmazások/fejvagyiras/fejvagyiras/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> proba = new List<string>();
+List<string> proba2 = new List<string>();
+
+StreamReader olvas = new StreamReader("kiserlet.txt");
+
+//While-os kiíratások
+while (!olvas.EndOfStream) 
+{
+    proba.Add(olvas.ReadLine());
+    
+}
+
+//For ciklusos kiíratások
+for (int i = 0; i < proba.Count; i++)
+{
+    Console.WriteLine(proba[i]);
+}
+
+for (int i = 0; i < proba2.Count; i++)
+{
+    Console.WriteLine(proba2[i]);
+}
+
