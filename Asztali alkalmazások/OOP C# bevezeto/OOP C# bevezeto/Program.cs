900a1ab242fd2bcb9488acaa8986bcee8917deb0
Tálas László Martin <talasl21@ADY-NAGYATAD.SULINET.HU>
2023-12-18 14:04:22 +0100

    karacsonyfa

--- Asztali alkalmazások/OOP C# bevezeto/OOP C# bevezeto/Program.cs
+++ Asztali alkalmazások/OOP C# bevezeto/OOP C# bevezeto/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class auto
+{
+    public int kerekSzam = 4;
+
+    public void dudal() 
+    {
+        Console.WriteLine("Tü Tűűűt!!!");
+    
+    }
+    private string tulaj;
+    public string tulajdonos 
+    {
+        get
+        {
+            return tulaj;
+        }
+
+        set
+        {
+            if (value == "Bence") 
+            {
+                Console.WriteLine("NEM!");
+
+            }
+            else 
+            {
+
+            }
+            tulaj = value;
+        }
+    
+    }
+}
+
+class Program 
+{
+    static void Main() 
+    {
+        auto kocsi = new auto();
+        Console.WriteLine(kocsi.kerekSzam);
+
+        kocsi.dudal();
+        kocsi.tulajdonos = "Gabi";
+        Console.WriteLine(kocsi.tulajdonos);
+        kocsi.tulajdonos = "Bence";
+        Console.WriteLine(kocsi.tulajdonos);
+
+    }
+
+
+}
