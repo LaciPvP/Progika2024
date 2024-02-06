3e25062c15eb96615443dbe5f7e51415cc996874
Tálas László Martin <talasl21@ady-nagyatad.sulinet.hu>
2024-02-06 13:55:42 +0100

    Abstract Class bevezetés

---
+++ Asztali alkalmazások/AbstractClass/AbstractClass/absztrakt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal abstract class absztrakt:Interface1
+    {
+        public int szam()
+        {
+            return 1000000;
+        }
+
+        public abstract void kiir();
+
+        int privatTusko = 0;
+        public int tusko 
+        {
+            get { return privatTusko; }
+            set { privatTusko = value; }
+        }
+    }
+}
