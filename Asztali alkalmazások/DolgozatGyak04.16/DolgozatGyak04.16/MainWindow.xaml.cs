642c3b4ac042906581eac8f7ecf4124c87c22ac8
Tálas László Martin <talasl21@ady-nagyatad.sulinet.hu>
2024-04-16 13:29:52 +0200

    dolgozat 04.16

---
+++ Asztali alkalmazások/DolgozatGyak04.16/DolgozatGyak04.16/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DolgozatGyak04._16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            combobox.Items.Add("Lány");
+            combobox.Items.Add("Fiú");
+
+            combobox1.Items.Add("Nagyatád");
+            combobox1.Items.Add("Kutas");
+            combobox1.Items.Add("Berzence");
+            combobox1.Items.Add("Segesd");
+            combobox1.Items.Add("Somogyszob");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listbox.Items.Add("Név: " + textbox1.Text);
+            listbox.Items.Add("Cím: " + textbox2.Text);
+            listbox.Items.Add("Életkor: " + textbox3.Text);
+            listbox.Items.Add("Neme: " + combobox.SelectedItem);
+            listbox.Items.Add("Lakhelye: " + combobox1.SelectedItem);
+        }
+    }
+}
--- Asztali alkalmazások/tortek/tortek/MainWindow.xaml.cs
+++ Asztali alkalmazások/tortek/tortek/MainWindow.xaml.cs
@@ -24,5 +24,52 @@ namespace tortek
         {
             InitializeComponent();
         }
+
+        private void textbox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int szamlalo = 0;
+            try
+            {
+                szamlalo = Convert.ToInt32(textbox1.Text);
+                
+            }
+
+            catch 
+            {
+            }
+
+
+            int nevezo = 0;
+            try
+            {
+                nevezo = Convert.ToInt32(textbox2.Text);
+
+            }
+
+            catch
+            {
+            }
+            if (nevezo != 0 && szamlalo % nevezo == 0)
+            {
+                textblock1.Text = (szamlalo / nevezo).ToString();
+            }
+
+            else if (nevezo != 0 && szamlalo % nevezo != 0)
+            {
+                textblock1.Text = "Nem egész";
+            }
+
+            else 
+            {
+                textblock1.Text = "Nullával való osztás";
+            }
+            
+
+        }
+
+        private void textbox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
     }
 }
